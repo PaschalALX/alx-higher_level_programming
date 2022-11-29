994b41250e7a39179c1fb725a5db67c87c238de1
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-11-29 19:06:26 +0000

    Task 1 ref * 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,4 +12,4 @@ elif last_number == 0:
     print(f"{last_number } and is 0")
 elif last_number < 6 or number < 0:
     last_number = -last_number if number < 0 else last_number
-    print(f"{last_number} and is less than 6")
+    print(f"{last_number} and is less than 6 and not 0")
