1eb026c3dbe7d1f105706e3614504a1af132049f
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-11-29 21:57:59 +0000

    Task 12 -ref * 1

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    [print ("FizzBuzz", end=" ") if not (x % 3) and not (x % 5)
-        else print ("Fizz", end=" ") if not (x % 3)
-            else print ("Buzz", end=" ") if not (x % 5) else print(x, end=" ") for x in range(1, 101)]
-
+    [print("FizzBuzz", end=" ") if not (x % 3) and not (x % 5)
+        else print("Fizz", end=" ") if not (x % 3)
+        else print("Buzz", end=" ") if not (x % 5)
+        else print(x, end=" ") for x in range(1, 101)]
