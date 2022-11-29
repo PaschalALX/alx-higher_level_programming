b808445d4980561c18e8f1cbb5a7f81d4a20261e
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-11-29 21:06:24 +0000

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for x in str:
+        if not (ord(x) >= 65 and ord(x) <= 90):
+            if (ord(x) >= 97 and ord(x) <= 122):
+                x = chr(ord(x) - 32)
+                print(x, end='')
+                print(end='\n')
