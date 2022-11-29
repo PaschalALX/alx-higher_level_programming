7bebea27cbfaa6cded5d1260592f4f3153c62fca
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-11-29 18:35:03 +0000

    First Commit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+print(number, "is positive") \
+    if number > 0 else print(number, "is zero") \
+            if number == 0 else print(number, "is negative")
