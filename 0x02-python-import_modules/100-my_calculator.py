4cc7041e1b1560d27e10a6e1e9ae75a2e5dd5a7a
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-01 22:53:44 +0000

    Added 100 task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    
+    from sys import argv, exit
+
+    if (len(argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if not (argv[2] in '+-*/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    
+    from calculator_1 import add, sub, mul, div
+ 
+    z = {
+        "x" : int(argv[1]),
+        "y" :  int(argv[3]),
+        "opr" : argv[2],
+        "res" : 0
+    }
+
+    if z['opr'] == '+':
+        z['res'] = add(z['x'], z['y'])
+    if z['opr'] == '-':
+        z['res'] = sub(z['x'], z['y'])
+    if z['opr'] == '*':
+        z['res'] = mul(z['x'], z['y'])
+    if z['opr'] == '/':
+        z['res'] = div(z['x'], z['y'])
+
+    print("{x} {opr} {y} = {res}".format(**z))
