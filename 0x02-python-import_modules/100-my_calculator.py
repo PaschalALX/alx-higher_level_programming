8646551fbbd02d31edd97f67d4be145034949561
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-01 22:56:18 +0000

    ref 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+
     from sys import argv, exit
 
     if (len(argv) != 4):
@@ -10,23 +10,23 @@ if __name__ == "__main__":
     if not (argv[2] in '+-*/'):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
+
     from calculator_1 import add, sub, mul, div
- 
+
     z = {
-        "x" : int(argv[1]),
-        "y" :  int(argv[3]),
-        "opr" : argv[2],
-        "res" : 0
+        "x": int(argv[1]),
+        "y":  int(argv[3]),
+        "opr": argv[2],
+        "res": 0
     }
 
     if z['opr'] == '+':
         z['res'] = add(z['x'], z['y'])
-    if z['opr'] == '-':
+    elif z['opr'] == '-':
         z['res'] = sub(z['x'], z['y'])
-    if z['opr'] == '*':
+    elif z['opr'] == '*':
         z['res'] = mul(z['x'], z['y'])
-    if z['opr'] == '/':
+    elif z['opr'] == '/':
         z['res'] = div(z['x'], z['y'])
 
     print("{x} {opr} {y} = {res}".format(**z))
