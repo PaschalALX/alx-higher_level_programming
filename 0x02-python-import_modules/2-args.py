5baa2f48fd7f634af8ed3df3076570cec931fb90
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-01 19:59:58 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# if __name__ == "__main__":
+
+from sys import argv
+
+x = len(argv) - 1
+y = "argument"
+
+print('{} {}'.format(x, y+":")) if x <= 1 else print('{} {}'.format(x, y+'s:'))
+[print('{}: {}'.format(z, argv[z])) for z in range(1, x + 1)]
