da2ecc40308555f680f8833e3f3bddcc84012224
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-01 20:18:54 +0000

    Task 2 ref * 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
 
-    from sys import argv
+from sys import argv
 
-    x = len(argv) - 1
-    y = "argument"
+x = len(argv) - 1
+y = "argument"
 
-    print('{} {}'.format(x, y+":"))\
-        if x <= 1 else print('{} {}'.format(x, y+'s:'))
-    [print('{}: {}'.format(z, argv[z])) for z in range(1, x + 1)]
+print('{} {}'.format(x, y+":")) \
+    if x == 1 else print('{} {}'.format(x, y+'.')) \
+    if x == 0 else print('{} {}'.format(x, y+'s:'))
+[print('{}: {}'.format(z, argv[z])) for z in range(1, x + 1)]
