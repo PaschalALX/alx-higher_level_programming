31fd3919acf7b1614c23a610853639fc7f8a446d
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-05 21:49:38 +0000

    Task 6 ref * 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
 
-    for x in matrix:
-        for y in range(len(x)):
-            if y >= len(x) - 1:
-                print("{:d}".format(x[y]))
-            else:
-                print("{:d}".format(x[y]), end=" ")
+    if matrix != [[]]:
+        for x in matrix:
+            for y in range(len(x)):
+                if y >= len(x) - 1:
+                    print("{:d}".format(x[y]))
+                else:
+                    print("{:d}".format(x[y]), end=" ")
+    else:
+        print()
