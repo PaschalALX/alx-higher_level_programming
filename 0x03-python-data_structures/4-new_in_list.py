428c48a8c619eab34188d52773d9dd60eabc2c6f
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-05 21:27:17 +0000

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if not my_list:
+        return None
+
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return new_list
