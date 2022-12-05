bdf5498ad47cd22daa5a21401eaa1f573ca121c2
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-05 21:39:18 +0000

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for y in range(len(x)):
+            if y >= len(x) - 1:
+                print("{:d}".format(x[y]))
+            else:
+                print("{:d}".format(x[y]), end=" ")
