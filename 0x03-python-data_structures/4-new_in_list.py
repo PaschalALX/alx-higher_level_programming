98cbcf07b2aad440368f3567be43a687e59d1646
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-12-05 21:30:41 +0000

    Task 4 ref * 1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,3 +7,6 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
         return new_list
+
+    new_list[idx] = element
+    return new_list
