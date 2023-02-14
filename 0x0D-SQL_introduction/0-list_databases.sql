f393242cbff9c5cf99b05f78356e3b4ef92582c8
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-02-14 18:04:22 +0000

    0 to 5

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all databases of your MySQL server.
+SHOW databases;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
+DROP database IF EXISTS hbtn_0c_0;
+
