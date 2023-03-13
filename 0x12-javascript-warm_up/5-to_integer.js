710dd0ac4f591b313f36791a0f2218f98b9658cd
PaschalALX <pasmac018@gmail.com>
2023-03-13 22:35:04 +0000

    Added task 5 R

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const { argv } = process;
 const number = parseInt(argv[2]);
 
 if (number || number === 0) {
-  console.log(number);
+  console.log(`My number: ${number}`);
 } else {
   console.log('Not a number');
 }
