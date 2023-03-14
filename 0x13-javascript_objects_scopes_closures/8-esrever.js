cf82d5ea713366c1ef360c3d42819f3ae018f4c7
PaschalALX <pasmac018@gmail.com>
2023-03-14 22:46:42 +0000

    Task 8 -RR

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const len = list.length;
-  for (let i = 0, j = len - 1; i < (len / 2); i++, j--) {
-    const temp = list[i];
-    list[i] = list[j];
-    list[j] = temp;
-  }
-  return list;
+  const newList = [];
+  list.forEach(element => newList.unshift(element));
+  return newList;
 };
