43d8e6d558270e715e404db7c433bd5a64d73ce1
PaschalALX <pasmac018@gmail.com>
2023-03-14 23:13:51 +0000

    Added Task 100 r

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const initialList = require('./100-data').list;
+const newList = initialList.map((elem, i) => elem * 1));
 console.log(initialList);
-console.log(initialList.map((elem, i) => elem * i));
+console.log(newList);
