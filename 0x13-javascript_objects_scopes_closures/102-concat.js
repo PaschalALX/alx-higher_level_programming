c844d71a7351988826af5e1f31465e374191f8c0
PaschalALX <pasmac018@gmail.com>
2023-03-15 00:19:00 +0000

    Tired

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,25 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const path = require('path');
-const { argv } = process;
-const argc = argv.length;
+let content = '';
 
-function exec (fileA, fileB, fileC) {
-  let output = '';
-  fs.readFile(path.resolve(fileA), 'utf-8', function (err, data) {
-    if (err) throw err;
-    output = output.concat(data, '\n');
-    fs.readFile(path.resolve(fileB), 'utf-8', function (err2, data2) {
-      if (err2) throw err2;
-      output = output.concat(data2, '\n');
-
-      fs.writeFile(path.resolve(fileC), output, function (err3) {
-        if (err3) throw err3;
-      });
-    });
-  });
-}
-
-if (argc === 5) {
-  exec(argv[2], argv[3], argv[4]);
-}
+content = content.concat(fs.readFileSync(process.argv[2]));
+content = content.concat(fs.readFileSync(process.argv[3]));
+fs.writeFileSync(process.argv[4], content);
