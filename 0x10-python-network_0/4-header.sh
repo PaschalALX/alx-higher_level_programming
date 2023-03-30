14ac09488f5f198272d1a58f6c166d4134f41dc7
PaschalALX <pasmac018@gmail.com>
2023-03-30 20:45:27 +0000

    Added More

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends Header
+curl -s -H "X-School-User-Id: 98" $1
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends Header
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" $1
