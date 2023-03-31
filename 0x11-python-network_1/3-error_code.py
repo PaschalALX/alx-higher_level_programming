ffc06a13f0635c9fc8527cc0d0b4d47a95b1ea67
PaschalALX <pasmac018@gmail.com>
2023-03-31 23:46:17 +0000

    E

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-'''
-Write a Python script that takes in a URL, sends a request
-to the URL and displays the body of the response
-(decoded in utf-8).
-'''
-
-if (__name__ == "__main__"):
-    from urllib import request, error
+"""
+Write a Python script that takes in a URL, sends a
+request to the URL and displays the body of the
+response (decoded in utf-8).
+"""
+if __name__ == "__main__":
+    import urllib.error as error
+    import urllib.request as request
     from sys import argv
-    if len(argv) == 2:
-        url = argv[1]
-        req_obj = request.Request(url)
-        try:
-            with request.urlopen(req_obj) as req:
-                print(req.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print('Error: {}'.format(e.code))
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
