71ebcd0cfa7e0d423001e1071a69ed72823dc0b8
PaschalALX <pasmac018@gmail.com>
2023-03-31 23:43:27 +0000

    D

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,13 @@ Write a Python script that takes in a URL, sends a request
 to the URL and displays the body of the response
 (decoded in utf-8).
 '''
-from urllib import request, error
-from sys import argv
 
 if (__name__ == "__main__"):
+    from urllib import request, error
+    from sys import argv
     if len(argv) == 2:
         url = argv[1]
-        req_ojb = request.Request(url)
+        req_obj = request.Request(url)
         try:
             with request.urlopen(req_obj) as req:
                 print(req.read().decode('utf-8'))
