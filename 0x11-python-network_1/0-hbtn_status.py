3831fcf735ce01edc2e64677d7b55325c0997c17
PaschalALX <pasmac018@gmail.com>
2023-03-31 21:46:29 +0000

    corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,14 @@
 '''
 Write a Python script that fetches https://alx-intranet.hbtn.io/status
 '''
-import urllib.request
 
 if __name__ == "__main__":
+    import urllib.request
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         res = response.read()
         _type = type(res)
         _utf8 = res.decode('utf-8')
 
         print("Body response:{3} type: {1}{3} content: {0}{3} utf8: {2}"
-              .format(res, _type, _utf8, '\n    -'))
+              .format(res, _type, _utf8, '\n\t-'))
 	
