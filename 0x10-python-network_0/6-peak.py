70ff0c8d16629e4dece0b5cbb2aa81b324c9942c
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-04-01 18:00:06 +0000

    Added task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,12 +5,12 @@
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
     li = list_of_integers
-    l = len(li)
-    if l == 0:
+    _l = len(li)
+    if _l == 0:
         return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+    m = _l // 2
+    if (m == _l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
         return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
+    if m != _l - 1 and li[m + 1] > li[m]:
         return find_peak(li[m + 1:])
     return find_peak(li[:m])
