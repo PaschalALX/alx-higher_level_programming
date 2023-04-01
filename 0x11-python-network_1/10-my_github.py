fdc570bd39a8c38f6c421d685ae0a96da9e0aba4
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2023-04-01 17:18:55 +0000

    Task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display
+your id
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) == 3:
+        username = argv[1]
+        password = argv[2]
+        git_user_url = "https://api.github.com/user"
+        headers = {
+            "Accept": "application/vnd.github+json"
+        }
+
+        res = requests.get(git_user_url, auth=(username, password), headers=headers)
+        print(res.json().get('id'))
