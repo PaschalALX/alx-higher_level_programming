bbfa79bf30df03beba32a9a7a382d245bd13ca7a
PaschalALX <pasmac018@gmail.com>
2023-04-25 22:33:26 +0000

    Added Task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (!error) { console.log('code: ', response.statusCode); }
+});
