d56fbd38219f8626fa2a1bb9def2d78ceee50b81
PaschalALX <pasmac018@gmail.com>
2023-04-25 22:35:35 +0000

    Refactored Tsk 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-  if (!error) { console.log('code: ', response.statusCode); }
+  if (!error) { console.log('code:', response.statusCode); }
 });
